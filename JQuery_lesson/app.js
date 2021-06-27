8de7a7cf3731ec2615f9889a206b7f00c697ba08
nao0725 <aqua.725.times@gmail.com>
2021-06-27 12:01:12 +0900

    Create PageTop-Lesson

---
+++ JQuery_lesson/app.js
@@ -0,0 +1,111 @@
+
+$(function(){
+  $('button').on('click', function(){
+    $('ul').children().css('color','red');
+  });
+});
+
+
+
+
+// $(function(){
+//   $(".box1").on("click",function(){
+//     $(this).slideUp();
+//   });
+// });
+
+
+// $(function(){
+//   $(".bg1").on("click",function(){
+//     $(".bg1").slideUp();
+//   });
+
+//   $(".bg2").on("click",function(){
+//     $(".bg2").slideUp();
+//   });
+
+//   $(".bg3").on("click",function(){
+//     $(".bg3").slideUp();
+//   });
+
+//   $(".bg4").on("click",function(){
+//     $(".bg4").slideUp();
+//   });
+// });
+
+// $(function(){
+//   $('.bg1').on('click', function(){
+//     $('.bg1').slideUp();
+//   });
+
+//   $('.bg2').on('click', function(){
+//     $('.bg2').slideUp();
+//   });
+
+//   $('.bg3').on('click', function(){
+//     $('.bg3').slideUp();
+//   });
+
+//   $('.bg4').on('click', function(){
+//     $('.bg4').slideUp();
+//   });
+// });
+
+
+
+
+
+// $(function(){
+//   $(".box1").on("click",function(){
+//     $(".box1").addClass("box1-ext");
+//   });
+//   $(".box1").mouseout(function(){
+//     $(".box1").removeClass("box1-ext");
+//   });
+// });
+
+
+
+
+
+// $(function(){
+//   $('.box1').slideDown(function (){
+//   $('.box1').css({
+//       'background-color': '#0000FF',
+//       'width': '200px',
+//       'height': '100px'
+//     }).slideUp();
+//   });
+// });
+
+
+  // $(function(){
+  //   $(".box1").slideDown(function(){
+  //   $(".box1").css({
+  //     'background-color': '#0000FF',
+  //     "height": "100px",
+  //     "width: 200px"
+  //   }).slideUp();
+  // });
+  // });
+
+
+
+// $(function(){
+//   $(".box1").hide();
+//   // $(".box1").css({'background-color': '#0000FF'});
+// });
+
+// $(function(){
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '100px'
+//   });
+// });
+
+// $(function(){
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '100px'
+//   });
+// });
